853abf54763236e5ddb42644607024b75c0118c9
Sunil <devsunilpatra@gmail.com>
2026-02-09 12:04:41 +0530

    initial commit

---
+++ server.ts
@@ -0,0 +1,19 @@
+
+import app from "./src/app"
+
+
+
+
+const startServer = () =>{
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+
+    console.log(`Server running on port ${port}`)
+})
+
+}
+
+
+startServer();
